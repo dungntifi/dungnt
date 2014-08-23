2862bcdaee7a322b06bdde306df54d479ebb8e61
Memfis <dmitrienko.alexander@gmail.com>
2014-08-23 16:50:31 +0900

    [#13166] Fixed gallery images saving

--- app/code/local/OpsWay/SimpleImages/Model/Observer.php
+++ app/code/local/OpsWay/SimpleImages/Model/Observer.php
@@ -12,35 +12,38 @@ class OpsWay_SimpleImages_Model_Observer
      */
     public function saveProductTabData(Varien_Event_Observer $observer)
     {
-             
-        $product = $observer->getEvent()->getProduct();
-        $childProducts = Mage::getModel('catalog/product_type_configurable')->getUsedProducts(null,$product);  
-     
-        try {
+        $simpleimagesColor = $this->_getRequest()->getPost('simpleimages_color');
+        $simpleimagesGallery = $this->_getRequest()->getPost('simpleimages_gallery');
 
-            $simpleimagesColor = $this->_getRequest()->getPost('simpleimages_color');
-            $simpleimagesGallery = $this->_getRequest()->getPost('simpleimages_gallery');
-                 
-            foreach($childProducts as $_simple){
-                if(isset($simpleimagesGallery[$_simple->getId()]) || isset($simpleimagesColor[$_simple->getId()])) {
-                    if (isset($simpleimagesColor[$_simple->getId()])){
-                        $_simple->setColor($simpleimagesColor[$_simple->getId()]);
-                    }
+        if(is_array($simpleimagesColor) || is_array($simpleimagesGallery)) {
+         
+            try {
 
-                    if(isset($simpleimagesGallery[$_simple->getId()])) {
-                        $_simple->setMediaGallery (array('images'=>array (), 'values'=>array ())); //media gallery initialization
-                        foreach ($simpleimagesGallery[$_simple->getId()] as $key => $value) {
-                            $_simple->addImageToMediaGallery('media/catalog/product' . $value, array('image','thumbnail','small_image'), false, false); //assigning image, thumb and small image to media gallery
+                $product = $observer->getEvent()->getProduct();
+                $childProducts = Mage::getModel('catalog/product_type_configurable')->getUsedProducts(null,$product);
+                     
+                foreach($childProducts as $_simple){
+                    if(isset($simpleimagesGallery[$_simple->getId()]) || isset($simpleimagesColor[$_simple->getId()])) {
+                        if (isset($simpleimagesColor[$_simple->getId()])){
+                            $_simple->setColor($simpleimagesColor[$_simple->getId()]);
                         }
-                    }
-                    $_simple->save();
-                }
-            }
 
-            $product->save();
-        }
-        catch (Exception $e) {
-            Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                        if(isset($simpleimagesGallery[$_simple->getId()])) {
+                            $_simple->setMediaGallery (array('images'=>array (), 'values'=>array ())); //media gallery initialization
+                            foreach ($simpleimagesGallery[$_simple->getId()] as $key => $value) {
+                                $_simple->addImageToMediaGallery('media/catalog/product' . $value, array('image','thumbnail','small_image'), false, false); //assigning image, thumb and small image to media gallery
+                            }
+                        }
+                        
+                        $_simple->save();
+                    }
+                }
+
+                $product->save();
+            }
+            catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
         }
     }
       
