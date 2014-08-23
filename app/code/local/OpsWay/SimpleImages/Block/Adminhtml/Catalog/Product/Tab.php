ca660496d8b20badcbff02f4a081bbacabd96fe3
Memfis <dmitrienko.alexander@gmail.com>
2014-08-23 16:39:50 +0900

    [#13166] Fixed simple products broken admin pages

--- app/code/local/OpsWay/SimpleImages/Block/Adminhtml/Catalog/Product/Tab.php
+++ app/code/local/OpsWay/SimpleImages/Block/Adminhtml/Catalog/Product/Tab.php
@@ -54,47 +54,4 @@ class OpsWay_SimpleImages_Block_Adminhtml_Catalog_Product_Tab extends Mage_Admin
         return false;
     }
  
-     
-    /**
-     * AJAX TAB's
-     * If you want to use an AJAX tab, uncomment the following functions
-     * Please note that you will need to setup a controller to recieve
-     * the tab content request
-     *
-     */
-    /**
-     * Retrieve the class name of the tab
-     * Return 'ajax' here if you want the tab to be loaded via Ajax
-     *
-     * return string
-     */
-#   public function getTabClass()
-#   {
-#       return 'my-custom-tab';
-#   }
- 
-    /**
-     * Determine whether to generate content on load or via AJAX
-     * If true, the tab's content won't be loaded until the tab is clicked
-     * You will need to setup a controller to handle the tab request
-     *
-     * @return bool
-     */
-#   public function getSkipGenerateContent()
-#   {
-#       return false;
-#   }
- 
-    /**
-     * Retrieve the URL used to load the tab content
-     * Return the URL here used to load the content by Ajax
-     * see self::getSkipGenerateContent & self::getTabClass
-     *
-     * @return string
-     */
-#   public function getTabUrl()
-#   {
-#       return null;
-#   }
- 
 }
