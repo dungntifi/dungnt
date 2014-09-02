f1ea46c93b72c8a4d503419b961b17c3925284f2
Memfis <dmitrienko.alexander@gmail.com>
2014-09-02 14:16:48 +0900

    [#13166] Added image type selection functionality

--- app/code/local/OpsWay/SimpleImages/Model/Observer.php
+++ app/code/local/OpsWay/SimpleImages/Model/Observer.php
@@ -12,26 +12,37 @@ class OpsWay_SimpleImages_Model_Observer
      */
     public function saveProductTabData(Varien_Event_Observer $observer)
     {
-        $simpleimagesColor = $this->_getRequest()->getPost('simpleimages_color');
-        $simpleimagesGallery = $this->_getRequest()->getPost('simpleimages_gallery');
+        $simpleImagesColor = $this->_getRequest()->getPost('simpleimages_color');
+        $simpleImagesGallery = $this->_getRequest()->getPost('simpleimages_gallery');
+        $simpleImagesType = $this->_getRequest()->getPost('simpleimages_type');
 
-        if(is_array($simpleimagesColor) || is_array($simpleimagesGallery)) {
+        if(is_array($simpleImagesColor) || is_array($simpleImagesGallery)) {
          
             try {
-
                 $product = $observer->getEvent()->getProduct();
                 $childProducts = Mage::getModel('catalog/product_type_configurable')->getUsedProducts(null,$product);
                      
                 foreach($childProducts as $_simple){
-                    if(isset($simpleimagesGallery[$_simple->getId()]) || isset($simpleimagesColor[$_simple->getId()])) {
-                        if (isset($simpleimagesColor[$_simple->getId()])){
-                            $_simple->setColor($simpleimagesColor[$_simple->getId()]);
+                    if(isset($simpleImagesGallery[$_simple->getId()]) || isset($simpleImagesColor[$_simple->getId()])) {
+                        if (isset($simpleImagesColor[$_simple->getId()])){
+                            $_simple->setColor($simpleImagesColor[$_simple->getId()]);
                         }
 
-                        if(isset($simpleimagesGallery[$_simple->getId()])) {
+                        if(isset($simpleImagesGallery[$_simple->getId()])) {
                             $_simple->setMediaGallery (array('images'=>array (), 'values'=>array ())); //media gallery initialization
-                            foreach ($simpleimagesGallery[$_simple->getId()] as $key => $value) {
-                                $_simple->addImageToMediaGallery('media/catalog/product' . $value, array('image','thumbnail','small_image'), false, false); //assigning image, thumb and small image to media gallery
+                            
+                            foreach ($simpleImagesGallery[$_simple->getId()] as $key => $value) {
+                                $currImageTypes = array();
+                                //check if current image has images type associated with it
+                                if(in_array($key, $simpleImagesType[$_simple->getId()])) {
+                                    foreach ($simpleImagesType[$_simple->getId()] as $imgType => $imgId) {
+                                        if($imgId == $key) {
+                                            array_push($currImageTypes, $imgType);
+                                        }
+                                    }
+                                }
+
+                                $_simple->addImageToMediaGallery('media/catalog/product' . $value, $currImageTypes, false, false); //assigning image types to media gallery
                             }
                         }
                         
