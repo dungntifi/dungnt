2a4bb19de647055dca07cf1d5e86afad43a36061
Nachshon Fishbane <nahshf@gmail.com>
2014-09-09 05:11:30 -0400

    anayltics change

---
+++ app/code/local/Fishbane/UGA/Block/Ga.php
@@ -0,0 +1,17 @@
+<?php
+class Fishbane_UGA_GoogleAnalytics_Block_Ga extends Mage_GoogleAnalytics_Block_Ga
+{
+   protected function _getPageTrackingCode($accountId)
+    {
+        $pageName   = trim($this->getPageName());
+        $optPageURL = '';
+        if ($pageName && preg_match('/^\/.*/i', $pageName)) {
+            $optPageURL = ", '{$this->jsQuoteEscape($pageName)}'";
+        }
+        return "
+  ga('create', '{$this->jsQuoteEscape($accountId)}', 'auto');
+  ga('require', 'displayfeatures');
+  ga('send', 'pageview');
+";
+    }
+}
