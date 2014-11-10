dfedf5ed0e5bcb431cc7cceff297d032cb197ebd
dungnt <dungnt@dtn.com.vn>
2014-11-10 13:02:21 +0700

    update sort by

--- app/code/local/Dtn/All/sql/dtnall_setup/mysql4-install-1.0.0.php
+++ app/code/local/Dtn/All/sql/dtnall_setup/mysql4-install-1.0.0.php
@@ -19,21 +19,36 @@ $installer->startSetup();
 //    ALTER TABLE sales_flat_shipment_item ADD COLUMN IF NOT EXISTS event_date DATETIME NULL;
 //");
 
-$installer->addAttribute('order_item','event_date',array(
-    'type' => 'DATETIME',
-    'nullable' => true,
-    'comment' => 'Event Date'
-));
-$installer->addAttribute('invoice_item','event_date',array(
-    'type' => 'DATETIME',
-    'nullable' => true,
-    'comment' => 'Event Date'
-));
-$installer->addAttribute('shipment_item','event_date',array(
-    'type' => 'DATETIME',
-    'nullable' => true,
-    'comment' => 'Event Date'
-));
+$installer->addAttribute(
+    'order_item',
+    'event_date',
+    array(
+        'type' => 'datetime',
+        'grid' => false,
+        'nullable' => true,
+        'comment' => 'Event Date'
+    )
+);
+$installer->addAttribute(
+    'invoice_item',
+    'event_date',
+    array(
+        'type' => 'datetime',
+        'grid' => false,
+        'nullable' => true,
+        'comment' => 'Event Date'
+    )
+);
+$installer->addAttribute(
+    'shipment_item',
+    'event_date',
+    array(
+        'type' => 'datetime',
+        'grid' => false,
+        'nullable' => true,
+        'comment' => 'Event Date'
+    )
+);
 
 $installer->updateAttribute('catalog_product', 'created_at', 'frontend_label', 'Date Added');
 $installer->updateAttribute('catalog_product', 'created_at', 'used_for_sort_by', '1');
